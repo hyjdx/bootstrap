2d6fafedf5dca6dd324164c54322940a80a35392
hyjdx <284063949@qq.com>
2015-11-22 14:16:43 +0800

    无

--- web/src/main/webapp/myjs/a.js
+++ web/src/main/webapp/myjs/a.js
@@ -2,7 +2,7 @@ var layout_left_width=190;
 window.onresize = function() {
     var widths = document.body.scrollWidth-layout_left_width;
     var heights = document.documentElement.clientHeight-38;
-    $("#content, #loading-frame").height(heights+25).width(widths-10);
+    $("#content").height(heights+25).width(widths-10);
     var heights = document.documentElement.clientHeight-38;
     $("#layout-left").height(heights+25);
 }
